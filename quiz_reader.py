97c941bb59a6bc8baafe95c0208085e99aa5052d
kenttyyy <kentajrins123@gmail.com>
2025-05-08 19:21:33 +0800

    module and color

--- quiz_reader.py
+++ quiz_reader.py
@@ -1,6 +1,17 @@
 # Program parts
+
 # Import necessary modules
+import random
+
 # Define ANSI color codes for output
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+CYAN = "\033[96m"
+MAGENTA = "\033[95m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
+
 # Define a function to load and parse quiz questions from file
 # Main program logic
 # Run the quiz
