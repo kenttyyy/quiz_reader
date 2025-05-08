8beb17f0618b0f6c88051f9bd22dcd1e417d0461
kenttyyy <kentajrins123@gmail.com>
2025-05-08 19:13:58 +0800

    program parts

---
+++ quiz_reader.py
@@ -0,0 +1,6 @@
+# Program parts
+# Import necessary modules
+# Define ANSI color codes for output
+# Define a function to load and parse quiz questions from file
+# Main program logic
+# Run the quiz
